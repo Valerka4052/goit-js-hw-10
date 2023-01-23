7f1155af2a237e59951622e1e3d6280b7b8fcd5f
Valerka4052 <114054150+Valerka4052@users.noreply.github.com>
2023-01-24 00:59:06 +0200

    add libraries

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
-console.log('hellobougou')
+
+import Notiflix from 'notiflix';
+var debounce = require('lodash.debounce');
+
+console.log(Notiflix);
+console.log(debounce);
